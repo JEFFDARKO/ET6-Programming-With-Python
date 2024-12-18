8d4641c3e8e479c7af04b2cfcf745a3b6892e9bf
JEFFDARKO <139300293+JEFFDARKO@users.noreply.github.com>
2024-12-18 00:18:02 +0000

    new start

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,23 @@
-def mystery_1(a,b):
+def mystery_1(a, b):
+    """ 
+    Takes two arguments and adds them together.
+
+    Arguments:
+        Both arguments, 'a' and 'b', can be float, int, or string.
+        a = int, float, string
+        b = int, float, string
+
+    Returns:
+        int, float, or str: The sum of `a` and `b`. If both are numbers, their sum is returned.
+                            If both are strings, their concatenation is returned.
+
+    Raises:
+        TypeError: if 'a' and 'b' are of different data types.
+
+    Example:
+        >>> mystery_1(3, 5)
+        8
+        >>> mystery_1("Hello, ", "world!")
+        'Hello, world!'
+    """
     return a + b
--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,4 +1,24 @@
 def mystery_2(a):
+    """ 
+    Returns the length of the input object, or None if it is empty.
+
+    Arguments:
+        a can be int or an iterable doc type: The input object whose length is to be determined. 
+        It can be a string, list, tuple, or other iterable object.
+
+    Returns:
+        int or None: 
+            - The length of the input object if it is non-empty.
+            - None if the input object is empty.
+
+    Example:
+        >>> mystery_2([1, 2, 3])
+        3
+        >>> mystery_2("Jeffery")
+        7
+        >>> mystery_2([1, 2])
+        2
+    """
     if len(a) == 0:
         return None
 
